466702372097c204d68de7250d0936cc5b9439c0
DinanG <gaoxuedinan@gmail.com>
2024-03-14 23:40:31 -0700

    update tests

--- toDoList/test/toDoList/LinkedListTest.java
+++ toDoList/test/toDoList/LinkedListTest.java
@@ -4,11 +4,36 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+//more complex functions will be tested in Driver
+
 public class LinkedListTest {
-
+	
+	LinkedList<Integer> test1 = new LinkedList<Integer>();
+	
+	// test count
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void countTest() {
+		test1.addNode(1);
+		test1.addNode(2);
+		test1.addNode(3);
+		assertEquals(3, test1.count());
 	}
-
+	
+	// test getNode
+	@Test
+	public void getNodeTest() {
+		test1.addNode(1);
+		test1.addNode(2);
+		test1.addNode(3);
+		assertEquals("3", test1.getNode(0));
+		assertEquals("2", test1.getNode(1));
+		assertEquals("1", test1.getNode(2));
+	}
+	
+	// test removeAll
+	@Test
+	public void removeAllNode() {
+		test1.removeAllNode();
+		assertEquals("", test1.toString());
+	}
 }
